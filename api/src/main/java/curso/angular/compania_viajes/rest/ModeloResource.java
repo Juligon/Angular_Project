dc72f17fbb2254de750c810d6b9edb14dd2c346a
juligon <julianagon73@gmail.com>
2023-07-01 14:45:17 -0300

    api funcionando

--- api/src/main/java/curso/angular/compania_viajes/rest/ModeloResource.java
+++ api/src/main/java/curso/angular/compania_viajes/rest/ModeloResource.java
@@ -7,25 +7,24 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(value = "/api/modelos", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ModeloResource {
 
-    private final ModeloService modeloService;
+  private final ModeloService modeloService;
 
-    public ModeloResource(final ModeloService modeloService) {
-        this.modeloService = modeloService;
-    }
+  public ModeloResource(final ModeloService modeloService) {
+    this.modeloService = modeloService;
+  }
 
-    @GetMapping
-    public ResponseEntity<List<ModeloDTO>> getAllModelos() {
-        return ResponseEntity.ok(modeloService.findAll());
-    }
+  @GetMapping
+  public ResponseEntity<List<ModeloDTO>> getAllModelos() {
+    return ResponseEntity.ok(modeloService.findAll());
+  }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ModeloDTO> getModelo(@PathVariable(name = "id") final Long id) {
-        return ResponseEntity.ok(modeloService.get(id));
-    }
+  @GetMapping("/{id}")
+  public ResponseEntity<ModeloDTO> getModelo(@PathVariable(name = "id") final Long id) {
+    return ResponseEntity.ok(modeloService.get(id));
+  }
 }
