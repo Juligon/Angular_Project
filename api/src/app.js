b6226584b5d8e965242aa047b72eda83eb259de1
juligon <julianagon73@gmail.com>
2023-08-01 21:03:32 -0300

    permissions policy

--- api/src/app.js
+++ api/src/app.js
@@ -14,12 +14,14 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.setHeader('Permissions-Policy', 'browsing-topics=(())');
   next();
 });
+
 
 server.use('/', routes);
 
